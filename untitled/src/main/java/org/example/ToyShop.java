2691e598f4812aeddeb0971704941bffbbc36b4d
DimaGrishin <amvlastik@gmail.com>
2023-06-22 13:49:45 +0300

    Создан класс ToyShop

---
+++ untitled/src/main/java/org/example/ToyShop.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class ToyShop {
+    int id;
+    String name;
+    int count;
+    int frequencyFalling;
+
+    public ToyShop(int id, String name, int count, int frequencyFalling) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+        this.frequencyFalling = frequencyFalling;
+    }
+}
